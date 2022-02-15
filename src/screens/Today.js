d416119bfe21f8558425bacd641b1c6fe398a981
Dante_Berishaj <db47425@ubt-uni.net>
2022-02-15 23:45:40 +0100

    adding colors from backend to categories

--- src/screens/Today.js
+++ src/screens/Today.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import Button from '../components/Button';
 import Card from '../components/Card';
 
 const Today = () => {
-  const [allTasks, setAllTasks] = useState(null)
+  const [categories, setCategories] = useState(null);
+  const [todayTasks, setTodayTasks] = useState(null);
+  const [showCat, setShowCat] = useState(false);
+  const [showToday, setShowToday] = useState(true);
 
-  const getAllTasks = async() => {
+  const getAllCategoriesAndTodayTasks = async() => {
     let token = localStorage.getItem('token')
-    //console.log(token)
-    await fetch('/tasks', {
+    await fetch('/dashboard', {
       method: "get",
       headers: {
         // eslint-disable-next-line no-eval
@@ -16,30 +19,60 @@ const Today = () => {
     }).then(response => {
       return response.json();
     }).then(data  => {
-      console.log(data.data)
-      setAllTasks(data.data)
+      console.log(data.data.lists)
+      setCategories(data.data.lists);
+      setTodayTasks(data.data.today_tasks);
     })
-    
    }
 
   useEffect(() => {
-    getAllTasks()
+    getAllCategoriesAndTodayTasks()
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  
+  const renderCategoriesHandler = () => {
+     setShowCat(true);
+     setShowToday(false)
+   }
+
+   const renderTodayHandler = () => {
+     setShowCat(false);
+     setShowToday(true)
+   }
 
   return (
     <>
-    <h1>Today Tasks</h1>
-    <div>
-      <h1>All tasks</h1>
-      {allTasks && allTasks.map((task) => (
-        <Card key={task.id}>
-          <h2>{task.name}</h2>
-          <h3>{task.due_date}</h3>
-          <h5>status: {task.completed === 1 ? "completed" : "not  completed"}</h5>
-        </Card>
-      ))}
+    {showToday && <div><h1>Today Tasks</h1>
+    <Button onClick={renderCategoriesHandler}>Show Categories</Button>
+    {todayTasks && todayTasks.map(today => (
+      <div></div>
+    ))}
     </div>
+    }
+    {showCat && <div>
+      <h1>All Categories</h1>
+      <Button onClick={renderTodayHandler}>Show Today List</Button>
+      <form>
+        <input placeholder='name' />
+      </form>
+      {categories && categories.map((cat) => (
+        <div 
+        key={cat.id} 
+        style={{backgroundColor: cat.color, 
+        width:'50%', 
+        justifyContent:'center', 
+        alignItems:'center', 
+        marginLeft: '25%', 
+        borderRadius: '12px', 
+        padding:'12px', 
+        marginTop:'3rem'
+        }}
+        >
+          <h2>{cat.name}</h2>
+          <h5>status: {cat.completed ? "completed" : "not  completed"}</h5>
+        </div>
+      ))}
+    </div>}
     </>
   );
 };

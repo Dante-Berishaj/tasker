e039aedcd52ebbd70db22293ec31e7659347a1c2
Dante_Berishaj <db47425@ubt-uni.net>
2022-02-15 18:38:07 +0100

    displaying all tasks with .map()

--- src/screens/Today.js
+++ src/screens/Today.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 const Today = () => {
-  const [allTasks, setAllTasks] = useState([]);
-
+  const [allTasks, setAllTasks] = useState(null)
 
   const getAllTasks = async() => {
     let token = localStorage.getItem('token')
-    console.log(token)
-    const response = await fetch('/tasks', {
+    //console.log(token)
+    await fetch('/tasks', {
       method: "get",
       headers: {
         // eslint-disable-next-line no-eval
@@ -15,24 +14,30 @@ const Today = () => {
       }
     }).then(response => {
       return response.json();
-      
+    }).then(data  => {
+      console.log(data.data)
+      setAllTasks(data.data)
     })
-    console.log(response.data);
-
-  
+    
    }
 
   useEffect(() => {
-
+    getAllTasks()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
     <h1>Today Tasks</h1>
-
     <div>
       <h1>All tasks</h1>
-      <button onClick={getAllTasks}>Token</button>
+      {allTasks && allTasks.map((task, index) => (
+        <div key={index}>
+          <h2>{task.name}</h2>
+          <h3>{task.due_date}</h3>
+          <h5>{task.completed}</h5>
+        </div>
+      ))}
     </div>
     </>
   );

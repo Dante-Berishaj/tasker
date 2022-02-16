dba013ad2e0218d1556cd9b91eff7afd7e5e6cf8
Dante_Berishaj <db47425@ubt-uni.net>
2022-02-16 21:14:29 +0100

    showing tasks to specific categories

--- src/screens/CategoryDetails.js
+++ src/screens/CategoryDetails.js
@@ -2,31 +2,57 @@
 import React, { useEffect, useState } from 'react'
 
 const CategoryDetails = (props) => {
-
   const catId = props.match.params.catId;
 
   const [currentCategory, setCurrentCategory] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const getCurrentCategory = async() => {
     let token = localStorage.getItem('token')
     await fetch(`/lists/${catId}`, {
       method: "get",
       headers: {
-        // eslint-disable-next-line no-eval
         "Authorization": `Bearer ` + token, 
       }
     }).then(response => {
       return response.json();
     }).then(data  => {
-      let categoryArr = data.data
+      const categoryArr = data.data
       setCurrentCategory(categoryArr)
     })
    };
 
+   const getTasks = async() => {
+    let token = localStorage.getItem('token')
+    await fetch('/tasks', {
+      method: "get",
+      headers: {
+        "Authorization": `Bearer ` + token, 
+      }
+    }).then(response => {
+      return response.json();
+    }).then(data  => {
+      const TasksArr = data.data;
+      setTasks(TasksArr);
+    })
+   }
+
 
   useEffect(() => {
     getCurrentCategory();
+    getTasks()
   }, []);
+
+  let catTasks = tasks;
+
+  console.log(catTasks)
+
+  let currTasks = catTasks.filter(task => {
+    // eslint-disable-next-line eqeqeq
+    return task.task_list_id == catId
+  })
+
+  console.log(currTasks)
 
   return (
     <>
@@ -38,6 +64,13 @@ const CategoryDetails = (props) => {
         }}
       >
         <h1>{currentCategory.name}</h1>
+        <h5>Tasks: {currentCategory.task_count}</h5>
+        {currTasks && currTasks.map(task => (
+          <div key={task.id}>
+            {task.name}
+          </div>
+        ))
+        }
       </div>
     </>
   );
